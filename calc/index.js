82ee182c139f65700fc67701e825eb8088d91c08
Kaavya Sarin <sarinkaavya19@gmail.com>
2026-04-14 17:21:36 +0530

    Add toggleParentheses function to calculator

--- calc/index.js
+++ calc/index.js
@@ -2,6 +2,17 @@ const display=document.getElementById("display");
 function appendToDisplay(input){
     display.value+=input;
 }
+function toggleParentheses(){
+    const currentValue = display.value;
+    const openCount = (currentValue.match(/\(/g) || []).length;
+    const closeCount = (currentValue.match(/\)/g) || []).length;
+    
+    if(openCount > closeCount){
+        display.value += ")";
+    } else {
+        display.value += "(";
+    }
+}
 function clearDisplay(){
     display.value="";
 }
